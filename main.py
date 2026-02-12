9b91c96ab6e8b3f1f459c9f8742518d37eeac10f
harsheyM <harshithamahesh421@gmail.com>
2026-02-12 11:53:27 -0600

    Added CRUD operations/endpoints

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from database import db
+from pydantic import BaseModel
+from reminder import create_reminder, get_reminders, get_reminder, update_reminder, delete_reminder
 
 app = FastAPI(title="Ask and Forget API")
 
@@ -18,3 +20,28 @@ def test_database():
         return {"status": "Success", "database": "Firestore Connected"}
     except Exception as e:
         return {"status": "Error", "message": str(e)}
+    
+class Reminder(BaseModel):
+    title: str
+    data: str
+    completed: bool = False
+    
+@app.post("/reminders")
+def api_create(reminder: Reminder):
+    return create_reminder(reminder.title, reminder.data)
+
+@app.get("/reminders")
+def api_read():
+    return get_reminders()
+
+@app.get("/reminders/{reminder_id}")
+def api_read_one(reminder_id: str):
+    return get_reminder(reminder_id)
+
+@app.put("/reminders/{reminder_id}")
+def api_update(reminder_id: str, updated_data: dict):
+    return update_reminder(reminder_id, updated_data)
+
+@app.delete("/reminders/{reminder_id}")
+def api_delete(reminder_id: str):
+    return delete_reminder(reminder_id)
