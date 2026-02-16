09793a3ec011d9f5c2763da397dd5793fce6f6d4
Christopher Wiratman <144767748+KaiJuSoftware789@users.noreply.github.com>
2026-02-16 01:05:50 -0600

    Updated auth.py
    Cleaned up some of the code and organized it as well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, Header
 from pydantic import BaseModel, EmailStr
-from firebase_admin import auth
+from firebase_admin import credentials, auth
 from database import db
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
@@ -11,15 +11,16 @@ from auth import signup as fb_signup, login as fb_login
 
 app = FastAPI(title="Ask and Forget API")
 
-bearer_scheme = HTTPBearer()
+
+# Gets bearer token from the Authorization header and verifies it with Firebase Admin SDK.
+# If valid, returns the decoded token (user info). If invalid, raises an HTTP 401 error.
+bearer = HTTPBearer()
 
 @app.get("/")
 def read_root():
     return {"status": "Active", "msg": "Ask and Forget backend is running."}
-    
-# Authentication
 
-# signup and login routes
+# Signup and Login
 
 class AuthBody(BaseModel):
     email: EmailStr
@@ -48,15 +49,26 @@ def login(body: AuthBody):
         }
     except ValueError as e:
         raise HTTPException(status_code=401, detail=str(e))
+
+
     
+    
+# Verification
 
 # For some odd reason, Swagger UI is not sending the Authorization header with the Bearer token, so
 # I have coded in an authorization http bearer to test authentication appropriately.
 
-def require_user(creds: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
-    return auth.verify_id_token(creds.credentials)
+def require_user(creds: HTTPAuthorizationCredentials = Depends(bearer)):
+    
+    try:
+        return auth.verify_id_token(creds.credentials)
+
+    except Exception:
+        # Catch anything unexpected
+        raise HTTPException(status_code=401, detail="Authentication failed")
 
 
+# Data extraction
 
 @app.get("/auth/me")
 def me(user=Depends(require_user)):
@@ -65,8 +77,6 @@ def me(user=Depends(require_user)):
         "email": user.get("email"),
         "email_verified": user.get("email_verified")
     }
-
-
 
 @app.get("/protected")
 def protected(user=Depends(require_user)):
